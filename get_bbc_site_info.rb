c1f6ed72010fe3ab02e7120a5221a30bf8e83243
William Daly <william.daly@bbc.co.uk>
2017-04-11 09:56:13 +0100

    making change to see if this will work now

--- get_bbc_site_info.rb
+++ get_bbc_site_info.rb
@@ -5,12 +5,12 @@ scanner = BBC.new
 iplayer_total = scanner.getIplayerTotalVpids()
 
 scanner.addHTMLheader()
-scanner.printNewsVpids( "News - Front Page"         , "http://trevor-producer.api.bbci.co.uk/content/cps/news/front_page"   	)
-scanner.printNewsVpids( "News - Most Popular"       , "http://trevor-producer.api.bbci.co.uk/content/most_popular/news" 	   	)
-scanner.printNewsVpids( "News - Technology"         , "http://trevor-producer.api.bbci.co.uk/content/cps/news/technology" 	  )
-scanner.printNewsVpids( "News - Politics"           , "http://trevor-producer.api.bbci.co.uk/content/cps/news/politics" 	    )
-scanner.printNewsVpids( "News - Mundo Front Page"   , "http://trevor-producer.api.bbci.co.uk/content/cps/mundo/front_page" 	  )
-scanner.printNewsVpids( "News - Russian Front Page" , "http://trevor-producer.api.bbci.co.uk/content/cps/russian/front_page"  )
+scanner.printNewsVpids( "News - Front Page"         , "http://trevor-producer.api.bbci.co.uk/content/cps/news/front_page"    )
+scanner.printNewsVpids( "News - Most Popular"       , "http://trevor-producer.api.bbci.co.uk/content/most_popular/news" 	   )
+scanner.printNewsVpids( "News - Technology"         , "http://trevor-producer.api.bbci.co.uk/content/cps/news/technology" 	 )
+scanner.printNewsVpids( "News - Politics"           , "http://trevor-producer.api.bbci.co.uk/content/cps/news/politics" 	   )
+scanner.printNewsVpids( "News - Mundo Front Page"   , "http://trevor-producer.api.bbci.co.uk/content/cps/mundo/front_page" 	 )
+scanner.printNewsVpids( "News - Russian Front Page" , "http://trevor-producer.api.bbci.co.uk/content/cps/russian/front_page" )
 
 scanner.printIplayerVpids( "iPlayer - Most Popular"  , "https://ibl.api.bbci.co.uk/ibl/v1/groups/popular/episodes"                                                 )
 scanner.printIplayerVpids( "iPlayer - Not Very Popular" , "https://ibl.api.bbci.co.uk/ibl/v1/groups/popular/episodes?per_page=20&page=#{(iplayer_total / 20) / 2}" )
