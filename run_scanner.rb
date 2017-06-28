0137416e4050c579d640a9e47a0846c2a66cbe3a
William Daly <william.daly@bbc.co.uk>
2017-06-28 16:57:02 +0100

    fixing parenthesis and making site responsive

--- run_scanner.rb
+++ run_scanner.rb
@@ -14,16 +14,16 @@ scanner.addHTMLheader()
 # More News content can be found at https://confluence.dev.bbc.co.uk/display/~jamie.pitts@bbc.co.uk/Trevor+Example+Endpoints
 # However I chose this links as they are not stale
 scanner.printNewsVpids( "News - Front Page"         , "http://trevor-producer.api.bbci.co.uk/content/cps/news/front_page"    )
-scanner.printNewsVpids( "News - Most Popular"       , "http://trevor-producer.api.bbci.co.uk/content/most_popular/news" 	   )
+scanner.printNewsVpids( "News - Most Popular"       , "http://trevor-producer.api.bbci.co.uk/content/most_popular/news" 	 )
 scanner.printNewsVpids( "News - Technology"         , "http://trevor-producer.api.bbci.co.uk/content/cps/news/technology" 	 )
-scanner.printNewsVpids( "News - Politics"           , "http://trevor-producer.api.bbci.co.uk/content/cps/news/politics" 	   )
+scanner.printNewsVpids( "News - Politics"           , "http://trevor-producer.api.bbci.co.uk/content/cps/news/politics" 	 )
 scanner.printNewsVpids( "News - Mundo Front Page"   , "http://trevor-producer.api.bbci.co.uk/content/cps/mundo/front_page" 	 )
 scanner.printNewsVpids( "News - Russian Front Page" , "http://trevor-producer.api.bbci.co.uk/content/cps/russian/front_page" )
 
 # More iPlayer content can be found at https://inspector.ibl.api.bbci.co.uk/
 scanner.printIplayerVpids( "iPlayer - Most Popular"     , "https://ibl.api.bbci.co.uk/ibl/v1/groups/popular/episodes"                                              )
 scanner.printIplayerVpids( "iPlayer - Not Very Popular" , "https://ibl.api.bbci.co.uk/ibl/v1/groups/popular/episodes?per_page=20&page=#{(iplayer_total / 20) / 2}" )
-scanner.printIplayerVpids( "iPlayer - Least Popular"    , "https://ibl.api.bbci.co.uk/ibl/v1/groups/popular/episodes?per_page=20&page=#{iplayer_total / 20}"		   )
+scanner.printIplayerVpids( "iPlayer - Least Popular"    , "https://ibl.api.bbci.co.uk/ibl/v1/groups/popular/episodes?per_page=20&page=#{iplayer_total / 20}"       )
 scanner.printGuidanceIplayerVpids( "iPlayer - Guidance" , "https://ibl.api.bbci.co.uk/ibl/v1/groups/popular/episodes?per_page=40&page=1" )
 
 # Other below options could include anything under the "Categories" dropdown on IBL
